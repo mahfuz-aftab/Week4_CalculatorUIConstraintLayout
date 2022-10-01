3659ec85f83cecdd335356eaa3f89cb28bdffc40
mahfuz-aftab <mahfuz.cracker@gmail.com>
2022-10-01 01:13:24 -0400

    Feat: Week4_CalculatorUIConstraintLayout

---
+++ app/src/main/java/com/mdev/week4_calculatoruiconstraintlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mdev.week4_calculatoruiconstraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
